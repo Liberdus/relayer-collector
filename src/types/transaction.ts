e992a9ebf8c7795578d1c0e30f2a411f7def0898
jairajdev <jairajranamagar.dev@gmail.com>
2025-02-04 23:29:00 +0700

    AppReceiptId fix and proper txFrom and txTo extraction from different txs

--- src/types/transaction.ts
+++ src/types/transaction.ts
@@ -4,7 +4,7 @@ export interface Transaction {
   timestamp: number
   cycleNumber: number
   data: any & { txId?: string; appReceiptId?: string }
-  originalTxData: unknown
+  originalTxData: unknown & { tx: any } // eslint-disable-line @typescript-eslint/no-explicit-any
   transactionType?: TransactionType
   txFrom?: string
   txTo?: string
